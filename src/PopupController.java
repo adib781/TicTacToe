2fcf1cd91bfcb55013df8594e495aa178a87135e
aliza-nyk <alizanadeem3@hotmail.com>
2022-08-02 16:28:42 -0400

    created popup.fxml to display outcome of game

---
+++ src/PopupController.java
@@ -0,0 +1,30 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+
+public class PopupController implements Initializable {
+
+    @FXML
+    private Label lbl1;
+    @FXML
+    private TextArea txtdetail;
+
+    // @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    @FXML 
+    public void setLabel(String s){
+        lbl1.setText(s);
+    }
+
+    
+    
+}
